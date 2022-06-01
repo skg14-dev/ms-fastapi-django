0575029fe1a84dd5d3eb18c8eb51dfc2d763e43e
skg14-dev <sean.kg@yp-uae.com>
2022-06-01 08:15:27 +0400

    adds jinja templating and base page

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+#very endpoint returns a json
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home_view():
+    return "<h1>helllo</h1>"
+    
+@app.post("/")
+def home_detail_view():
     return {"hello":"world"}
