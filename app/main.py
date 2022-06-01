69719a2672406c8d440abae37e5818fdd19c274f
skg14-dev <sean.kg@yp-uae.com>
2022-06-01 08:46:53 +0400

    adds possibility of passing values from backend

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
-#very endpoint returns a json
+templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
+#every endpoint returns a json by default
 
 @app.get("/", response_class=HTMLResponse)
-def home_view():
-    return "<h1>helllo</h1>"
+#need to mention what the variable type is.
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html",{"request":request,"test":123})
     
 @app.post("/")
 def home_detail_view():
